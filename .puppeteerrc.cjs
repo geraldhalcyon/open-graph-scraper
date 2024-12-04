66bf5a4a8c2a856cd8367013e84d2dad3acb81bb
geraldhalcyon <geraldsayson.halcyondigital@gmail.com>
2024-12-04 18:04:01 +0800

    testing

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -5,5 +5,13 @@ const { join } = require("path");
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
+  chrome: {
+    skipDownload: false,
+  },
+  // Download Firefox (default `skipDownload: true`).
+  firefox: {
+    skipDownload: false,
+  },
+
   cacheDirectory: join(__dirname, ".cache", "puppeteer"),
 };
